4e0139a47fb3b8dc6c2b457214a23b87c51266bb
srk-kh <155601885+srk-kh@users.noreply.github.com>
2024-10-03 08:45:37 +0000

    initial version

---
+++ src/Posts.jsx
@@ -0,0 +1,10 @@
+import {useParams, useLocation } from "react-router-dom";
+
+export default function  Posts(){
+
+    const {id} = useParams();
+    const urlstring = new URLSearchParams(useLocation().search);
+    const fname=urlstring.get("fname");
+    const lname=urlstring.get("lname");
+    return (<h1>This is Posts page Hello {fname} {lname} {id} </h1>);
+}
